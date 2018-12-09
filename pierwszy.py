83a713679bfb2305cb00ee38e8dbd2b8419a01b3
patkar2018 <patkar2018@users.noreply.github.com>
2018-12-09 10:10:58 +0100

    pierwszy program

---
+++ pierwszy.py
@@ -0,0 +1,15 @@
+ocena_5 = 12
+ocena_4 = 6
+ocena_3 = 3
+ocena_2 = 1
+wiek = 8.5
+
+nazwa = 'Klasa IVB'
+
+print ("Witaj " + nazwa)
+
+print ("Piatke otrzymalo:" + " " +  str(ocena_5) + " " + "osob")
+print ("Czworke otrzymalo:" + " " +  str(ocena_4) + " " + "osob")
+print ("Trojke otrzymalo:" + " " + str(ocena_3) + " " + "osob")
+print ("Dwojke otrzymalo:" + " " + str(ocena_2) + " " + "osob")
+print ("Sredni Wiek Dzieci:" + " " + str(wiek) + " ")
