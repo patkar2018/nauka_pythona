b3ef87a5cbd013326f3abc0337056c61909c8b9a
patkar2018 <patkar2018@users.noreply.github.com>
2018-12-09 13:02:21 +0100

    czwarty 0912

---
+++ czwarty.py
@@ -0,0 +1,10 @@
+samochody = ['syrena', 'poloneza']
+
+print(samochody[0])
+print(samochody[1])
+
+for s in samochody:
+    print(s)
+
+for idx in range (len (samochody) ):
+    print ("idx: " + str(idx) + " : " + samochody[idx])
