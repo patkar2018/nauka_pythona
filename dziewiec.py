16eb0f1fa9304e1d1240077b3055bcb0bb45fd6d
patkar2018 <patkar2018@users.noreply.github.com>
2018-12-09 14:26:02 +0100

    druga wersja

--- dziewiec.py
+++ dziewiec.py
@@ -29,17 +29,19 @@ else:
 print("Do zaplacenia" + " " + str(suma))
 
 #########sprawdza czy mleko i ser wystepuja###################################################
-stan_reguly = {'mleko' : False,
-                'ser': False}
+bylo_mleko = False
+byl_ser = False
+
 suma = 0
 
 for poz in koszyk:
      suma = suma + poz['cena']
-     nazwa_prod = poz['nazwa']
-     if nazwa_prod == 'mleko' or nazwa_prod == 'ser':
-         stan_reguly[nazwa_prod] = True
+     nazwa_prod = poz['name']
+     if nazwa_prod == 'mleko':
+         bylo_mleko = True
+     if nazwa_prod == 'ser':
+         byl_ser = True
 
-if stan_reguly['mleko']:
-    print ("Znizka")
-    suma = suma - (suma *10) /100
+if bylo_mleko and byl_ser:
+        suma = suma - (suma *10) /100
 print (suma)
